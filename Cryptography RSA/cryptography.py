1e1f3b1133a92d141aa0170ba072d355d554af3e
Guido Marinelli <72472738+GuidoMarinelli@users.noreply.github.com>
2022-01-14 21:46:57 +0100

    Update cryptography.py

--- Cryptography RSA/cryptography.py
+++ Cryptography RSA/cryptography.py
@@ -4,10 +4,10 @@ decrypting a message passed as a txt file."""
 import random
 import numpy as np
 
-e = 65537  # Default value of the public key exponent.
+e = 65537  # Default value of the public key exponent
 file_extension = '.txt'
 
-# Import a list of prime numbers from a CSV file.
+# Import a list of prime numbers from a CSV file
 dataset = np.genfromtxt(  
     'primesSet.csv',
     delimiter=',',
@@ -17,7 +17,7 @@ dataset = np.genfromtxt(
 def rsa_coding_system():
     """Execution of the RSA coding system."""
 
-    # Takes a file as input and displays it.
+    # Takes a file as input and displays it
     while True:
         try:
             file_name = input('Enter the name of file to be encoded: ')
@@ -32,22 +32,22 @@ def rsa_coding_system():
             print('\nTEXT PRESENT IN THE FILE:')
             print(str_file)
 
-            # Create a list of text characters in the file.
+            # Create a list of text characters in the file
             m_list = []
             for char in str_file:
                 m_list += char
 
-            # Randomly select two prime numbers from the dataset.
+            # Randomly select two prime numbers from the dataset
             p = random.choice(dataset)
             q = random.choice(dataset)
 
-            # Calculate the modulus.
+            # Calculate the modulus
             n = p * q
 
-            # Calculate the tozient function.
+            # Calculate the tozient function
             f = (p - 1) * (q - 1)
 
-            # Determines the exponent d of the private key.
+            # Determines the exponent d of the private key
             d = 0
             i = 1
             condition = True
@@ -58,12 +58,12 @@ def rsa_coding_system():
                     break
                 i = i + 1
 
-            # View the keys.
+            # View the keys
             print('\nKEYS:')
             print(f'Public key: {n, e}')
             print(f'Private key: {n, d}')
 
-            # Performs the encryption process.
+            # Performs the encryption process
             encrypted_text = ''
             c_list = []
             for char in m_list:
@@ -74,21 +74,21 @@ def rsa_coding_system():
                 else:
                     encrypted_text += str((m ** e) % n)
 
-            # View the contents of the encrypted file.
+            # View the contents of the encrypted file
             print('\nENCRYPTED FILE:')
             print(encrypted_text)
 
-            # Save the encrypted file as encrypted_filename.txt.
+            # Save the encrypted file as encrypted_filename.txt
             encrypted_file = 'encrypted_' + file
             with open(encrypted_file, mode='w') as c_file:
                 c_file.write(encrypted_text)
 
-            # Performs the decryption process.
+            # Performs the decryption process
             decrypted_text = ''
             for c in c_list:
                 decrypted_text += (chr((c ** d) % n))
 
-            # View the contents of the dencrypted file.
+            # View the contents of the dencrypted file
             print('\nCHECK THE PROCESS:\n(decrypted file)')
             print(decrypted_text)
             break
